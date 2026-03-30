59fb9717e764c3886071299e4cc91ac3d0f82c1f
premiumhaircarebrand <premium.hair.care.brand@gmail.com>
2026-03-30 23:30:47 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,8 +16,8 @@ export default function Footer() {
         </span>
         <div className="divider-golden flex-1 hidden md:block mx-12" />
         <div className="flex items-center gap-8">
-          {links.map(({ label, href })) => (
-            
+          {links.map(({ label, href }) => (
+            <a>
               key={label}
               href={href}
               target={href.startsWith("http") ? "_blank" : undefined}
